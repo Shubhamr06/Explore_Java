86fc9eba4c651d1db7cd8a0763f1d46ebb9d9725
Shubhamr06 <32364476+Shubhamr06@users.noreply.github.com>
2020-08-19 23:18:59 +0530

    Add files via upload
    In this code we can get know , what is max and min size of 8 data type which is almost fixed and we called them primitive type

---
+++ Premitive.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myminINTvalue= Integer.MIN_VALUE;
+        int mymaxINTvalue= Integer.MAX_VALUE;
+        System.out.println("The min value of int= "  +myminINTvalue);
+        System.out.println("The max value of int= "  +mymaxINTvalue);
+
+        byte myminbytevalue= Byte.MIN_VALUE;
+        byte mymaxbytevalue= Byte.MAX_VALUE;
+        System.out.println("The min value of int= "  +myminbytevalue);
+        System.out.println("The max value of int= "  +mymaxbytevalue);
+    }
+}
