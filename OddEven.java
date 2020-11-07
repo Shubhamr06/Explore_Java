8d6ece6769369dca6970301f75d2c685fefb9566
Shubhamr06 <32364476+Shubhamr06@users.noreply.github.com>
2020-11-07 22:22:10 +0530

    Find the odd and even no with the given number

---
+++ OddEven.java
@@ -0,0 +1,20 @@
+package com.rai.shubham;
+
+public class OddEven {
+
+    public static void main(String[] args)
+    {
+
+        System.out.println("Here we are printing even and odd no");
+
+        int n=3;
+        if (n%2==0)
+        {
+            System.out.println("n is even");
+        }
+        else
+        {
+            System.out.println("n is odd");
+        }
+    }
+}
