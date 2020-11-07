bbda339ec241dbf320e5afbb2f14f1413e2c56cf
Shubhamr06 <32364476+Shubhamr06@users.noreply.github.com>
2020-11-07 22:19:07 +0530

    Add files via upload

---
+++ SwitchEx.java
@@ -0,0 +1,23 @@
+package com.rai.shubham;
+
+public class SwitchEx {
+
+    public static void main(String[] args)
+
+    {
+        int num=30;
+        switch (num){ //expression
+            case 0:
+                System.out.println("zero");
+                break;
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            default:
+                System.out.println("invalid value");
+        }
+    }
+}
