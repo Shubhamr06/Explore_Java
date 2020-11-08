209f7745c1991de0a72e9afd3a3b765346833316
Shubhamr06 <32364476+Shubhamr06@users.noreply.github.com>
2020-11-08 16:05:00 +0530

    Basic working of DoWhile loop
    It is basic flow of do while loop which will show that the do while loop will execute at least one time even after conditions fails.

---
+++ DoWhileEx.java
@@ -0,0 +1,13 @@
+package com.rai.shubham;
+
+public class DoWhileEx {
+
+    public static void main(String[] args){
+
+        int i =15;
+        do {
+            System.out.println("Vaule of i is " + i); // This loop will execute atleast one time.
+            i++;
+        }while (i<=10);
+    }
+}
