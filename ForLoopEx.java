28d961b6cdb4b31acc222d9165c9858595ccdd09
Shubhamr06 <32364476+Shubhamr06@users.noreply.github.com>
2020-11-08 16:15:53 +0530

    For loop example

---
+++ ForLoopEx.java
@@ -0,0 +1,11 @@
+package com.rai.shubham;
+
+public class ForLoopEx {
+
+    public static void main(String[] args){
+
+        for (int i=0; i<=10; i++){
+            System.out.println("2 * " + i + " = " + 2*i);
+        }
+    }
+}
